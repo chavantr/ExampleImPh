dee02d0455745164dfb4c766880edb06c806cda8
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2018-03-26 21:47:33 +0530

    initial login

--- app/src/main/java/com/ahmedadeltito/photoeditor/RegistrationActivity.java
+++ app/src/main/java/com/ahmedadeltito/photoeditor/RegistrationActivity.java
@@ -2,7 +2,11 @@ package com.ahmedadeltito.photoeditor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -11,11 +15,43 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText txtEmail;
     private EditText txtPassword;
     private EditText txtAddress;
+    private Spinner spnGender;
+    private TextView lblDateOfBirth;
+    private Button btnRegister;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtMobileNumber = (EditText) findViewById(R.id.txtPhonerNumber);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        txtAddress = (EditText) findViewById(R.id.txtAddress);
+        spnGender = (Spinner) findViewById(R.id.spnGender);
+        lblDateOfBirth = (TextView) findViewById(R.id.lblDateOfBirth);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()){
+
+                }else {
+
+                }
+            }
+        });
+
+
+    }
+
+    private boolean validate() {
+
+
+
+        return false;
     }
 }
